9c5e2f21632361d9d01f3309da013ab425e79535
Brittany Bellanca <brittanybellanca@gmail.com>
2018-06-16 18:50:52 -0400

    added functions per step #6

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// function declaration
+function howdy() {
+    console.log('Brittany Bellanca says hey there!');
+}
+
+howdy();
+
+// function expression
+var numba = function ourFunction(string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    }
+
+    else if (string.length > 7) {
+        console.log('I’m sorry, but that’s too many to count.');
+    }
+
+    else {
+        console.log('7, what a perfect choice!');
+    }
+}
+
+numba('7');
+
+numba('seventy');
+
+numba('university');
+
+
+// Function with Parameters
+function inception(display, favMovie) {
+    display(favMovie);
+}
+
+function display(favMovie) {
+    console.log('Pitch Perfect is a fantastic movie.');
+}
+
+inception(display);
