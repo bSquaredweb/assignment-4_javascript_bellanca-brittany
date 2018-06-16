77a8c2586585b21277f2a5863dc3595be7e5e1e3
Brittany Bellanca <brittanybellanca@gmail.com>
2018-06-16 16:04:20 -0400

    added president information per step #5

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var president1 = {
+    firstName: 'Donald',
+    lastName: 'Trump',
+    termLength: '2',
+    party: 'Republican',
+    yearsOfPresidency: '2017-present'
+};
+
+var president2 = {
+    firstName: 'Barack',
+    lastName: 'Obama',
+    termLength: '8',
+    party: 'Democratic',
+    yearsOfPresidency: '2009-2017'
+};
+
+var president3 = {
+    firstName: 'George W.',
+    lastName: 'Bush',
+    termLength: '8',
+    party: 'Republican',
+    yearsOfPresidency: '2001-2009'
+};
+
+var president4 = {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: '8',
+    party: 'Democratic',
+    yearsOfPresidency: '1993-2001'
+};
+
+var president5 = {
+    firstName: 'George H.W.',
+    lastName: 'Bush',
+    termLength: '4',
+    party: 'Republican',
+    yearsOfPresidency: '1989-1993'
+};
+
+var presidents = [president1, president2, president3, president4, president5];
+
+console.log(president3.firstName + ' ' + president3.lastName);
